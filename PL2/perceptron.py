5a754342aca653b67d8468de68575202bf0e89cb
ZhanXDD <elgor2k1@gmail.com>
2023-03-16 15:06:27 +0100

    tmp push

--- PL2/perceptron.py
+++ PL2/perceptron.py
@@ -2,13 +2,13 @@
 # -------------
 
 
-
 # Perceptron implementation
 import util
 import pickle
 import DataLoad
 
 PRINT = True
+
 
 class PerceptronClassifier:
     """
@@ -17,21 +17,20 @@ class PerceptronClassifier:
     Note that the variable 'datum' in this code refers to a counter of features
     (not to a raw samples.Datum).
     """
-    def __init__( self, legalLabels, max_iterations):
+
+    def __init__(self, legalLabels, max_iterations):
         self.legalLabels = legalLabels
         self.type = "perceptron"
-        self.max_iterations = 5 # max_iterations
+        self.max_iterations = 5  # max_iterations
         self.weights = {}
         for label in legalLabels:
-            self.weights[label] = util.Counter() # this is the data-structure you should use
+            self.weights[label] = util.Counter()  # this is the data-structure you should use
 
     def setWeights(self, weights):
         assert len(weights) == len(self.legalLabels)
         self.weights = weights
 
-    
-        
-    def train( self, trainingData, trainingLabels, validationData, validationLabels):
+    def train(self, trainingData, trainingLabels, validationData, validationLabels):
         """
         The training loop for the perceptron passes through the training data several
         times and updates the weight vector for each label based on classification errors.
@@ -42,38 +41,38 @@ class PerceptronClassifier:
         datum is a counter from features to values for those features
         (and thus represents a vector a values).
         """
-      
-        trainingData=DataLoad.loadTrainingData()
-        trainingLabels=DataLoad.loadTrainingLabels()
-        self.features = trainingData[0].keys() # could be useful later
+
+
+
+        trainingData = DataLoad.loadTrainingData()
+        trainingLabels = DataLoad.loadTrainingLabels()
+
+        self.features = trainingData[0].keys()  # could be useful later
         # DO NOT ZERO OUT YOUR WEIGHTS BEFORE STARTING TRAINING, OR
         # THE AUTOGRADER WILL LIKELY DEDUCT POINTS.
-        
         print("Length trainingData: ", len(trainingData))
         print("Length trainingLabels: ", len(trainingLabels))
 
         for iteration in range(self.max_iterations):
-            print ("Starting iteration ", iteration, "...")
-            
-            
-        
-        
-    def classify(self, data ):
+            print("Starting iteration ", iteration, "...")
+            score = 0
+            for index in range(len(trainingData)):
+                sum = 0
+                for i in trainingData[index]:
+                    sum += trainingData[index].get(i) * self.weights[trainingLabels[index]][i]
+                score = max(score, sum)
+
+    def classify(self, data):
         """
         Classifies each datum as the label that most closely matches the prototype vector
         for that label.  See the project description for details.
 
         Recall that a datum is a util.counter...
         """
-        
-        data=DataLoad.loadValidationData()
-             
+
+        data = DataLoad.loadValidationData()
+
         guesses = []
-        #for i in range(len(data)):
-           
-
-
+        # for i in range(len(data)):
 
         return guesses
-    
-
